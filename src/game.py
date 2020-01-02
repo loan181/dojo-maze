e7f9bc20e7a3a3d9c1021ddcaba8a23c2d4e86cf
Frederic Marchal <fmarchal@perso.be>
2020-01-02 21:54:23 +0100

    Add pygame view of the maze while the user moves around.

--- src/game.py
+++ src/game.py
@@ -2,7 +2,11 @@ from main import step
 from maze import Maze
 from player import Player, DIRECTION
 from tile import TILE
-
+try:
+    from view import View
+    HAVE_VIEW = True
+except ImportError:
+    HAVE_VIEW = False
 
 class Game:
     def __init__(self, maze_str, debug=False, stepByStep=False):
@@ -12,16 +16,24 @@ class Game:
         self._action_point = 0
         self._maze = Maze(maze_str)
         self._player: Player = self._maze.getPlayer()
+        if self._debug and HAVE_VIEW:
+            self._view = View(self._maze)
+        else:
+            self._view = None
 
     def startGame(self):
         self._stepN = 0
         self._action_point = 0
         self._won = False
+        if self._view is not None:
+            self._view.draw(self._stepN, self._action_point)
 
         for self._stepN in range(1, 1000):
             step(self) # User defined
 
             if self._debug:
+                if self._view is not None:
+                    self._view.draw(self._stepN, self._action_point)
                 self.printMaze()
 
             if self._maze.isPlayerOnGoal():
@@ -72,5 +84,10 @@ class Game:
             print("Etape {} ({} actions): ".format(self._stepN, self._action_point))
             self._maze.printMaze()
             if self._stepByStep:
-                input("( press enter to continue ... )")
+                if self._view is not None:
+                    self._view.wait_key()
+                else:
+                    input("( press enter to continue ... )")
+            elif self._view is not None:
+                self._view.escape_pressed()
             print()
